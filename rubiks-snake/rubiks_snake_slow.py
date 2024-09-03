8b7e141c1f08675b588d01896943cee3782dd1d8
Dmytro Fedoriaka <fedimser@gmail.com>
2024-09-02 19:30:01 -0700

    move code

--- rubiks-snake/rubiks_snake_slow.py
+++ rubiks-snake/rubiks_snake_slow.py
@@ -1,9 +1,58 @@
+from rubiks_snake import *
 
 
-def construct_formula():
-    pass
-def is_formula_valid():
-    pass
+class Wedge:
+    def __init__(self, coord, wedge_id):
+        self.coord = coord
+        self.wedge_id = wedge_id
 
-def enumerate_valid_formulas_slow():
-    pass
+    def __repr__(self):
+        x = self.coord % BOX_SIZE
+        y = (self.coord // BOX_SIZE) % BOX_SIZE
+        z = (self.coord // BOX_SIZE) // BOX_SIZE
+        f1, f2 = WEDGE_ID_TO_FACE_IDS[self.wedge_id]
+        return f"({x},{y},{z};{f1}->{f2})"
+
+    def get_next(self, rot):
+        return Wedge(self.coord + WEDGE_ID_TO_NEXT_DELTA[self.wedge_id],
+                     ROT_AND_WEDGE_ID_TO_NEXT_WEDGE_ID[self.wedge_id + 36 * rot])
+
+    def occ_type(self):
+        return self.wedge_id & 15
+
+
+def construct_formula(formula: list[int]):
+    """Returns wedges (if formula is valid), or None if it's invalid."""
+    wedges = [Wedge(CENTER_COORD, FACE_IDS_TO_WEDGE_ID[(0, 3)])]
+    cubes = {wedges[0].coord: wedges[0].occ_type()}
+    assert len(formula) + 1 <= MAX_N
+    for rot in formula:
+        next_wedge = wedges[-1].get_next(rot)
+        if next_wedge.coord not in cubes:
+            cubes[next_wedge.coord] = next_wedge.occ_type()
+        else:
+            assert cubes[next_wedge.coord] > 0
+            can_push = (cubes[next_wedge.coord] + next_wedge.occ_type() == 13)
+            if not can_push:
+                return None
+            cubes[next_wedge.coord] += next_wedge.occ_type()
+            assert cubes[next_wedge.coord] == 13
+        wedges.append(next_wedge)
+    return wedges
+
+
+def is_formula_valid(formula):
+    if type(formula) is str:
+        formula = list(map(int, formula))
+    return construct_formula(formula) is not None
+
+
+def enumerate_valid_formulas_slow(n):
+    ans = []
+    rots = np.zeros(n - 1, dtype=np.int64)
+    for i in range(4 ** (n - 1)):
+        for j in range(n - 1):
+            rots[j] = (i >> (2 * j)) & 3
+        if is_formula_valid(rots):
+            ans.append("".join(map(str, rots)))
+    return ans
--- rubiks-snake/rubiks_snake_test.py
+++ rubiks-snake/rubiks_snake_test.py
@@ -1,13 +1,29 @@
+import pytest
+
 from rubiks_snake import RubiksSnakeCounter
+from rubiks_snake_slow import enumerate_valid_formulas_slow
 
 
 def test_count_shapes():
     counts = RubiksSnakeCounter.count_all_shapes(16)
     for i in range(1, 16):
         assert counts[i - 1] == RubiksSnakeCounter.S[i]
+    for i in range(1, 8):
+        assert counts[i - 1] == len(enumerate_valid_formulas_slow(i))
 
 
 def test_palindrome_shapes():
     assert RubiksSnakeCounter.count_palindrome_shapes(8) == 221
     assert RubiksSnakeCounter.count_palindrome_shapes(9) == 185
     assert RubiksSnakeCounter.count_palindrome_shapes(10) == 802
+
+
+@pytest.mark.parametrize("n", [1, 2, 3, 4, 5, 6, 7, 8])
+def test_palindromes_slow(n):
+    valid_shapes = enumerate_valid_formulas_slow(n)
+    num_shapes = len(valid_shapes)
+    num_palindromes = sum(1 for s in valid_shapes if s[::-1] == s)
+    num_shapes_up_to_reverse = len(set(min(s, s[::-1]) for s in valid_shapes))
+    assert num_shapes == RubiksSnakeCounter.S[n]
+    assert num_palindromes == RubiksSnakeCounter.count_palindrome_shapes(n)
+    assert 2 * num_shapes_up_to_reverse == num_palindromes + num_shapes
