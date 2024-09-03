71c97c5a991b7a30b21192c391a171bd488c0174
Dmytro Fedoriaka <fedimser@gmail.com>
2024-09-02 19:16:31 -0700

    Update notebook

--- rubiks-snake/rubiks_snake_test.py
+++ rubiks-snake/rubiks_snake_test.py
@@ -2,6 +2,12 @@ from rubiks_snake import RubiksSnakeCounter
 
 
 def test_count_shapes():
-    counts = RubiksSnakeCounter.count_shapes_all(16)
+    counts = RubiksSnakeCounter.count_all_shapes(16)
     for i in range(1, 16):
         assert counts[i - 1] == RubiksSnakeCounter.S[i]
+
+
+def test_palindrome_shapes():
+    assert RubiksSnakeCounter.count_palindrome_shapes(8) == 221
+    assert RubiksSnakeCounter.count_palindrome_shapes(9) == 185
+    assert RubiksSnakeCounter.count_palindrome_shapes(10) == 802
